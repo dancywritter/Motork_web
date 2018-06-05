5f93e49e818772bb0e120c7717908e71a61dd0f3
hawks87 <harreen123@gmail.com>
2018-06-05 20:53:37 +0500

    changes in webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,63 @@
 const path = require('path');
 
+/*module.exports = {
+    mode: 'production',
+    entry: './src/js/app.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ]
+            }
+        ]
+    }
+}*/
+
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  entry: './src/js/app.js',
+  entry: ['./src/js/app.js', './scss/main.scss'],
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'build')
+    filename: 'build/app.js'
   },
-  watch: true,
+  mode: 'production',
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "style-loader" // creates style nodes from JS strings
-          },
-          {
-            loader: "css-loader" // translates CSS into CommonJS
-          },
-          {
-            loader: "sass-loader" // compiles Sass to CSS
-          }
-        ]
+      /*
+      your other rules for JavaScript transpiling go in here
+      */
+      { // regular css files
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader?importLoaders=1',
+        }),
+      },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
       }
     ]
-  }
-}
+  },
+  plugins: [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'build/main.bundle.css',
+      allChunks: true,
+    }),
+  ],
+};
