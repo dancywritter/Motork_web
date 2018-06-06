ec07b95a5aef93da3dcfd4eae0bfe2e00667e0ad
hawks87 <harreen123@gmail.com>
2018-06-06 23:25:30 +0500

    changes made in gallery and responsiveness

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
-/*module.exports = {
+module.exports = {
     mode: 'production',
-    entry: './src/js/app.js',
+    entry: ['./src/js/app.js', './src/scss/main.scss'],
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, 'build')
@@ -26,38 +26,4 @@ const path = require('path');
             }
         ]
     }
-}*/
-
-
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-module.exports = {
-  entry: ['./src/js/app.js', './scss/main.scss'],
-  output: {
-    filename: 'build/app.js'
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      /*
-      your other rules for JavaScript transpiling go in here
-      */
-      { // regular css files
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          loader: 'css-loader?importLoaders=1',
-        }),
-      },
-      { // sass / scss loader for webpack
-        test: /\.(sass|scss)$/,
-        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin({ // define where to save the file
-      filename: 'build/main.bundle.css',
-      allChunks: true,
-    }),
-  ],
-};
+}
